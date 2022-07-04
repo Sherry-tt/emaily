af77c54d5fcc9e397a134ea40054aeb6749c2ff1
rtan <793346994@qq.com>
2022-07-04 16:03:16 -0400

    update surveyTemplate

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,26 +1,6 @@
 const keys = require('../../config/keys');
 <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a> 
 
-// module.exports = survey => {
-//   return `
-//     <html>
-//       <body>
-//         <div style="text-align: center;">
-//           <h3>I'd like your input!</h3>
-//           <p>Please answer the following question:</p>
-//           <p>${survey.body}</p>
-//           <div>
-//               <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a> 
-//           </div>
-//           <div>
-//             <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
-//           </div>
-//         </div>
-//       </body>
-//     </html>
-//   `;
-// };
-
 module.exports = survey => {
   return `
     <html>
@@ -30,14 +10,16 @@ module.exports = survey => {
           <p>Please answer the following question:</p>
           <p>${survey.body}</p>
           <div>
-              <a href="${keys.redirectDomain}api/surveys/${survey.id}/yes">Yes</a> 
+              <a href="${keys.redirectDomain}/api/surveys/${survey.id}/yes">Yes</a> 
           </div>
           <div>
-            <a href="${keys.redirectDomain}api/surveys/${survey.id}/no">No</a>
+            <a href="${keys.redirectDomain}/api/surveys/${survey.id}/no">No</a>
           </div>
         </div>
       </body>
     </html>
   `;
 };
+
+
 
